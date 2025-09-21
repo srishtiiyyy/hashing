0ea871c4b3b35041643c3e149a092762ad3894c2
Srishti <srishtisingh910@gmail.com>
2025-09-21 19:36:33 +0530

    INTEGER TO ROMAN

---
+++ integertoroman.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string intToRoman(int num) {
+        vector<int> values = {
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4, 1
+        };
+
+        vector<string> symbols = {
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV", "I"
+        };
+
+        string result = "";
+        for (int i = 0; i < values.size(); ++i) {
+            while (num >= values[i]) {
+                result += symbols[i];
+                num -= values[i];
+            }
+        }
+        return result;
+    }
+};
+
+int main() {
+    int num;
+    cout << "Enter an integer: ";
+    cin >> num;
+
+    Solution sol;
+    string roman = sol.intToRoman(num);
+
+    cout << "Roman numeral: " << roman << endl;
+
+    return 0;
+}
